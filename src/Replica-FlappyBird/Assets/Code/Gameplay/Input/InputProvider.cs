1b159f23fe4dcb258b4f6207aebe9249dddb66b1
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2025-12-24 09:44:48 -0500

    Input created. Player Burst created.

---
+++ src/Replica-FlappyBird/Assets/Code/Gameplay/Input/InputProvider.cs
@@ -0,0 +1,16 @@
+﻿using Code.Gameplay.Movement;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Code.Gameplay.Input
+{
+  public class InputProvider : MonoBehaviour
+  {
+    [SerializeField] private PlayerMovement _playerMovement;
+
+    public void OnBurst(InputValue value)
+    {
+      _playerMovement.FlyingBurst();
+    }
+  }
+}
---
+++ src/Replica-FlappyBird/Assets/Code/Gameplay/Movement/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Code.Gameplay.Movement
+{
+  public class PlayerMovement : MonoBehaviour
+  {
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _force;
+
+    public void FlyingBurst()
+    {
+      _rigidbody.linearVelocity = Vector2.up * _force;
+    }
+  }
+}
